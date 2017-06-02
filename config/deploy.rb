8e783fcc0e39a229ce55efaf5ea7c628b7524a7b
PIL_aboozar <rajabi.aboozar@gmail.com>
2017-06-02 14:57:43 +0200

    git address is changed to univ-lehavre

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.8.1"
 
 set :application, "cirmar"
-set :repo_url, "git@github.com:ab00zar/cirmar.git"
+set :repo_url, "git@forgeb1.univ-lehavre.fr:rajabia/cirmar-interface.git" 
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
